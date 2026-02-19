aeca2595f6df6c4b2b96d34cb177f4d02fba582c
nkululekomandla36-netizen <nkululekomandla36@gmail.com>
2026-02-19 04:50:05 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 // Sinyoro App JS
+
 console.log("Sinyoro App loaded");
+
+// Select the button
+const greetBtn = document.getElementById('greetBtn');
+
+// Add a click event
+greetBtn.addEventListener('click', () => {
+    alert("Welcome to Sinyoro!");
+});
